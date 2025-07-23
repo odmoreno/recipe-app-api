fd2a2d27f30ce9d13b3607e71630d9243b21cb36
Oscar Moreno <oscarpol95@gmail.com>
2025-07-23 14:19:52 -0500

    test: add debug print statements in wait_for_db tests for better tracing

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,6 +16,7 @@ class CommandTests(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db if db ready."""
+        print("patched_check", patched_check)
         patched_check.return_value = True
         call_command("wait_for_db")
         patched_check.assert_called_once_with(databases=['default'])
@@ -23,6 +24,7 @@ class CommandTests(SimpleTestCase):
     @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting OperationalError."""
+        print("patched_check", patched_check)
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
