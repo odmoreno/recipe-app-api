3e601a8f37e2774ff83d209df64f6e0a236a4048
Oscar Moreno <oscarpol95@gmail.com>
2025-07-30 11:03:37 -0500

    feat(user): implement user creation API with serializers and URL routing

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -30,7 +30,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-
+        print('res 1:', res)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
@@ -45,7 +45,7 @@ class PublicUserApiTests(TestCase):
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
-
+        print('res2 :', res)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
@@ -56,7 +56,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-
+        print('res 3:', res)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
